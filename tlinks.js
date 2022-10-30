e04bdf8c149f89708d5eabe3b94e49fb49d6ad76
Erghel <mysterium1337@gmail.com>
2022-10-30 21:36:02 +0300

    Update

--- tlinks.js
+++ tlinks.js
@@ -1,7 +1,7 @@
 // tlinks script
 
-let anchors = document.getElementsByTagName("a");
+let links = document.getElementsByTagName("a");
 
-for (let i = 0; i < anchors.length; i++) {
-    anchors[i].href = anchors[i].href.replace("t.me", "tlinks.run");
+for (let i = 0; i < links.length; i++) {
+    links[i].href = links[i].href.replace("t.me", "tlinks.run");
 }
